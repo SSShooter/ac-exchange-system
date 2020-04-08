93697af95dbb9c04e44219b787f1ecc375b07512
ssshooter <472356884@qq.com>
2020-04-08 22:50:48 +0800

    更新axios拦截器

--- src/api.js
+++ src/api.js
@@ -1,4 +1,18 @@
 import axios from 'axios'
+import { Notify } from 'vant'
+axios.interceptors.response.use(
+  function(response) {
+    // 2xx
+    console.log('response', response)
+    return response
+  },
+  function(error) {
+    // 非 2xx
+    if (error.response.data) {
+      return Notify({ type: 'danger', message: error.response.data.msg })
+    } else return Notify({ type: 'danger', message: '网络错误' })
+  }
+)
 export function register(data) {
   axios.post('/ac/api/auth/register', data)
 }
