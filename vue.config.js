fcf127ef8157742158a041d6316329f7518c6a21
zhoudejie <zhoudj94@gmail.com>
2020-04-14 11:46:36 +0800

    配置更新

--- vue.config.js
+++ vue.config.js
@@ -26,13 +26,6 @@ module.exports = {
       skipWaiting: true,
       clientsClaim: true
     },
-    iconPaths: {
-      favicon32: '/favicon-32x32.png',
-      favicon16: '/favicon-16x16.png',
-      appleTouchIcon: '/apple-touch-icon.png',
-      maskIcon: '/safari-pinned-tab.svg',
-      msTileImage: '/mstile-150x150.png'
-    },
     manifestOptions: {
       background_color: '#FFFFFF',
       icons: [
